c234732023f6cd521c0b2565c5378353eb6b34d8
岩浆 <linboyan5438081@163.com>
2024-08-20 22:17:36 +0800

    feat: levi帮助

---
+++ config/help/help.js
@@ -0,0 +1,262 @@
+/**
+* 请注意，系统不会读取help_default.js ！！！！
+* 【请勿直接修改此文件，且可能导致后续冲突】
+*
+* 如需自定义可将文件【复制】一份，并重命名为 help.js
+*
+* */
+
+export const helpCfg = {
+  title: 'levi帮助',
+  subTitle: 'Karin & levi',
+  colCount: 4,
+  colWidth: 265,
+  theme: 'all',
+  themeExclude: ['default'],
+  style: {
+    fontColor: '#ceb78b',
+    descColor: '#eee',
+    contBgColor: 'rgba(6, 21, 31, .5)',
+    contBgBlur: 3,
+    headerBgColor: 'rgba(6, 21, 31, .4)',
+    rowBgColor1: 'rgba(6, 21, 31, .2)',
+    rowBgColor2: 'rgba(6, 21, 31, .35)'
+  }
+}
+
+export const helpList = [
+  {
+    group: '娱乐',
+    list: [
+      {
+        title: '#开枪',
+        desc: '俄罗斯转盘开枪'
+      }, {
+        title: '#结束游戏',
+        desc: 'Game Over'
+      }
+    ]
+  }, {
+    group: '文字',
+    list: [
+      {
+        title: 'v50',
+        desc: '疯狂星期四'
+      }, {
+        title: '污句子',
+        desc: '随机污句子'
+      }, {
+        title: '随机日记',
+        desc: '返回各种日记'
+      }, {
+        title: '舔狗日记',
+        desc: '返回舔狗日记'
+      }, {
+        title: '新春祝福',
+        desc: '拜个早年'
+      }, {
+        title: '网易云热评',
+        desc: '返回一条网易云热评'
+      }
+    ]
+  }, {
+    group: '照片',
+    list: [
+      {
+        title: '萌宠',
+        desc: '可爱萌宠'
+      }, {
+        title: 'mc酱',
+        desc: 'mc酱'
+      }, {
+        title: '兽猫酱',
+        desc: '兽猫酱'
+      }, {
+        title: '集原美',
+        desc: '集原美'
+      }, {
+        title: '甘城',
+        desc: '甘城猫猫'
+      }
+    ]
+  }, {
+    group: '表情包',
+    list: [
+      {
+        title: '狐狐',
+        desc: '狐狐'
+      }, {
+        title: '废柴',
+        desc: '废柴'
+      }, {
+        title: '龙图',
+        desc: '随机龙图'
+      }, {
+        title: 'a梦',
+        desc: '哆啦A梦'
+      }, {
+        title: '咖波',
+        desc: '可爱咖波捏'
+      }, {
+        title: '小豆泥',
+        desc: '小豆泥'
+      }, {
+        title: '蘑菇头',
+        desc: '蘑菇头表情包'
+      }, {
+        title: '熊猫头',
+        desc: '熊猫头表情包'
+      }, {
+        title: '派大星',
+        desc: '派大星表情包'
+      }, {
+        title: '小黄鸡',
+        desc: '小黄鸡表情包'
+      }, {
+        title: '小灰灰',
+        desc: '小灰灰'
+      }, {
+        title: '小恐龙',
+        desc: '小恐龙'
+      }, {
+        title: '随机柴郡',
+        desc: '可爱柴郡（超了）'
+      }, {
+        title: '一二布布',
+        desc: '一二布布表情包'
+      }
+    ]
+  }, {
+    group: '好康的',
+    list: [
+      {
+        title: 'mt',
+        desc: '腿子'
+      }, {
+        title: 'ak',
+        desc: 'ak制服'
+      }, {
+        title: 'cos',
+        desc: 'cosplay，图较少'
+      }, {
+        title: 'JK',
+        desc: 'JK'
+      }, {
+        title: 'waifu',
+        desc: '嘿嘿嘿~老婆'
+      }, {
+        title: '汉服',
+        desc: '汉服小姐姐'
+      }, {
+        title: '国风',
+        desc: '古装国风'
+      }, {
+        title: '黑丝',
+        desc: '黑丝捏'
+      }, {
+        title: '白丝',
+        desc: '白丝捏'
+      }, {
+        title: '小性感',
+        desc: '小性感'
+      }, {
+        title: '小姐姐',
+        desc: '小姐姐捏'
+      }, {
+        title: '买家秀',
+        desc: '买家秀捏'
+      }, {
+        title: '夏日女友',
+        desc: '夏日女友'
+      }, {
+        title: '微博美女',
+        desc: '微博美女'
+      }, {
+        title: '随机ai',
+        desc: '随机ai图片'
+      }, {
+        title: 'loli',
+        desc: 'loli视频'
+      }, {
+        title: '甜妹',
+        desc: '甜妹视频'
+      }, {
+        title: '抖音（快手）变装',
+        desc: '抖音、快手变装视频'
+      }, {
+        title: '随机小姐姐',
+        desc: '甜妹视频'
+      }, {
+        title: '双倍快乐',
+        desc: '分屏快乐'
+      }, {
+        title: '玉足',
+        desc: '玉足、美腿视频'
+      }, {
+        title: '随机裙子',
+        desc: 'JK、洛丽塔视频'
+      }, {
+        title: 'cos系列',
+        desc: 'coser'
+      }, {
+        title: '慢摇视频',
+        desc: '摇'
+      }, {
+        title: '纯情女高',
+        desc: '挺好看的'
+      }, {
+        title: '吊带系列',
+        desc: '有点......'
+      }, {
+        title: '完美身材',
+        desc: '没看出来'
+      }, {
+        title: '热舞视频',
+        desc: '一般般'
+      }, {
+        title: '穿搭系列',
+        desc: '凑合'
+      }
+    ]
+  }, {
+    group: '其他',
+    list: [
+      {
+        title: '随机唱鸭',
+        desc: '随机返回唱歌语音'
+      }, {
+        title: '随机坤坤',
+        desc: '坤坤语音'
+      }, {
+        title: '随机网易云',
+        desc: '随机返回一首歌'
+      }, {
+        title: '骂我',
+        desc: '日语骂人'
+      }, {
+        title: '随机绿茶',
+        desc: '绿茶语音'
+      }
+    ]
+  }, {
+    group: '工具',
+    list: [
+      {
+        title: '#ping ip(域名)',
+        desc: 'ping(不显示ip)'
+      }, {
+        title: '#Ping ip(域名)',
+        desc: 'Ping(显示ip)'
+      }, {
+        title: '#sendPic+链接',
+        desc: '只支持返回类型为图片的接口网址'
+      }, {
+        title: '#sendVid+链接',
+        desc: '只支持返回类型为视频的接口网址'
+      }, {
+        title: '#sendRec+链接',
+        desc: '只支持返回类型为语音的接口网址'
+      }
+    ]
+  }
+]
